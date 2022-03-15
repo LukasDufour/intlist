7397ff9550dc5b87b6381bc04ae873a232b7e075
Dufour <Dufour@DESKTOP-5452IJF>
2022-03-15 12:44:41 +0100

    implementation using a field 'int[] elements'

--- intlist/src/intlist/IntList.java
+++ intlist/src/intlist/IntList.java
@@ -3,44 +3,67 @@ package intlist;
 import java.util.Arrays;
 
 public class IntList {
+	/**
+	 * @invar | elements != null
+	 * 
+	 * @representationObject
+	 */
+	private int[] elements;
 	
 	/**
 	 * @post | result != null
 	 * 
 	 * @creates | result
 	 */
-	public int[] getElements() {}
+	public int[] getElements() { return elements.clone(); }
 	
 	/** 
 	 * @post | result == getElements().length
 	 */
-	public int getLength() {}
+	public int getLength() { return elements.length; }
 	
 	/**
 	 * 
 	 * @pre | 0 <= index && index < getLength()
 	 * @post | result == getElements()[index]
+	 * 
+	 * @inspects | this
 	 */
-	public int getElementAt(int index) {}
+	public int getElementAt(int index) { return elements[index]; }
 	
 	/**
 	 * @post | getLength() == 0
 	 */
-	public IntList() {}
+	public IntList() {
+		elements = new int[0];
+	}
 
 	/**
 	 * @post | getLength() == old(getLength()) + 1
 	 * @post | getElementAt(old(getLength())) == value
 	 * @post | Arrays.equals(old(getElements()), 0, old(getLength()), getElements(), 0, old(getLength()))
+	 * 
+	 * @mutates | this
 	 */
-	public void add(int value) {}
+	public void add(int value) {
+		int[] newElements = new int[elements.length + 1];
+		System.arraycopy(elements, 0, newElements, 0, elements.length);
+		newElements[elements.length] = value;
+		elements = newElements;
+	}
 	
 	/**
 	 * @pre | getLength() > 0
 	 * 
 	 * @post | getLength() == old(getLength()) - 1
 	 * @post | Arrays.equals(old(getElements()), 0, getLength(), getElements(), 0, getLength())
+	 * 
+	 * @mutates | this
 	 */
-	public void removeLast() {}
+	public void removeLast() {
+		int[] newElements = new int[elements.length - 1];
+		System.arraycopy(elements, 0, newElements, 0, newElements.length);
+		elements = newElements;
+	}
 	
 }
